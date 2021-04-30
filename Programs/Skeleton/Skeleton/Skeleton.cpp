9b9cfd1fac432c6fcbb206d7f04c9f42eee67089
triszt4n <trisztanpiller@gmail.com>
2021-04-30 15:27:48 +0200

    :pear: Everything fine except the energy conservation

--- Programs/Skeleton/Skeleton/Skeleton.cpp
+++ Programs/Skeleton/Skeleton/Skeleton.cpp
@@ -46,19 +46,57 @@ template<class T> struct Dnum {
 		return Dnum(f / r.f, (r.f * d - r.d * f) / r.f / r.f);
 	}
 };
-
-template<class T> Dnum<T> Exp(Dnum<T> g) { return Dnum<T>(expf(g.f), expf(g.f)*g.d); }
 template<class T> Dnum<T> Sin(Dnum<T> g) { return  Dnum<T>(sinf(g.f), cosf(g.f)*g.d); }
 template<class T> Dnum<T> Cos(Dnum<T>  g) { return  Dnum<T>(cosf(g.f), -sinf(g.f)*g.d); }
-template<class T> Dnum<T> Tan(Dnum<T>  g) { return Sin(g) / Cos(g); }
-template<class T> Dnum<T> Sinh(Dnum<T> g) { return  Dnum<T>(sinh(g.f), cosh(g.f)*g.d); }
-template<class T> Dnum<T> Cosh(Dnum<T> g) { return  Dnum<T>(cosh(g.f), sinh(g.f)*g.d); }
-template<class T> Dnum<T> Tanh(Dnum<T> g) { return Sinh(g) / Cosh(g); }
-template<class T> Dnum<T> Log(Dnum<T> g) { return  Dnum<T>(logf(g.f), g.d / g.f); }
 template<class T> Dnum<T> Pow(Dnum<T> g, float n) { return  Dnum<T>(powf(g.f, n), n * powf(g.f, n - 1) * g.d); }
-
 typedef Dnum<vec2> Dnum2;
-const int tessellationLevel = 100;
+
+const int TESSELATION_LEVEL = 100;
+const vec3 GRAVITY_VEC = { 0, 0, -9.81f };
+const float R_NULL = 0.005f;
+const float AMPLIFIER = 15.0f;
+const float UPPER_BOUND = 10.0f;
+const float FOV_DEGREES = 45.0f;
+const float SPHERE_RADIUS = 0.5f;
+const float STARTING_MASS = 0.05f;
+const float SPHERE_MASS = 1.0f;
+const float MAX_DEPTH = -12.0f;
+
+struct Weight {
+	Dnum2 X, Y;
+	float mass;
+
+	Weight(float _mass, Dnum2 _X, Dnum2 _Y) : mass(_mass), X(_X), Y(_Y) { }
+};
+std::vector<Weight> weights;
+
+float rnd() { return (float)rand() / RAND_MAX; }
+bool operator==(vec3 a, vec3 b) { return a.x == b.x && a.y == b.y && a.z == b.z; }
+vec3 operator/(vec3 a, vec3 b) { return { a.x / b.x, a.y / b.y, a.z / b.z }; }
+
+Dnum2 h(Dnum2 X, Dnum2 Y) {
+	Dnum2 sum;
+	for (auto& weight : weights) {
+		Dnum2 sqrtRes = Pow(Pow(X - weight.X, 2) + Pow(Y - weight.Y, 2), 0.5f);
+		sum = sum + Dnum2(weight.mass) / (sqrtRes + Dnum2(R_NULL));
+	}
+	return sum * (-1);
+}
+
+vec4 qmul(vec4 q1, vec4 q2) {
+	vec3 d1 = { q1.x, q1.y, q1.z };
+	vec3 d2 = { q2.x, q2.y, q2.z };
+	vec3 res = d2 * q1.w + d1 * q2.w + cross(d1, d2);
+	return { res.x, res.y, res.z, q1.w * q2.w - dot(d1, d2) };
+}
+
+vec4 qinv(vec4 q) {
+	return vec4(-q.x, -q.y, -q.z, q.w) / dot(q, q);
+}
+
+vec4 quat(float t) {
+	return { sinf(t / 4) * cosf(t) / 2, sinf(t / 4) * sinf(t) / 2, sinf(t / 4) * sqrtf(3 / 4), cosf(t / 4) };
+}
 
 struct Camera {
 	vec3 wEye, wLookat, wVup;
@@ -66,8 +104,9 @@ struct Camera {
 public:
 	Camera() {
 		asp = (float)windowWidth / windowHeight;
-		fov = 45.0f * (float)M_PI / 180.0f;
-		fp = 1; bp = 45;
+		fov = FOV_DEGREES * (float)M_PI / 180.0f;
+		fp = 1;
+		bp = UPPER_BOUND * 10;
 	} 
 	mat4 V() {
 		vec3 w = normalize(wEye - wLookat);
@@ -80,62 +119,51 @@ public:
 	}
 
 	mat4 P() {
-		return mat4(1 / (tan(fov / 2)*asp), 0,                0,                      0,
-			        0,                      1 / tan(fov / 2), 0,                      0,
-			        0,                      0,                -(fp + bp) / (bp - fp), -1,
-			        0,                      0,                -2 * fp*bp / (bp - fp),  0);
+		return mat4(1 / (tanf(fov / 2)*asp), 0,                 0,                       0,
+			        0,                       1 / tanf(fov / 2), 0,                       0,
+			        0,                       0,                 -(fp + bp) / (bp - fp), -1,
+			        0,                       0,                 -2 * fp*bp / (bp - fp),  0);
 	}
 };
 
 struct Material {
 	vec3 kd, ks, ka;
-	float shininess;
+	float shine;
+
+	Material(vec3 _kd, vec3 _ks, vec3 _ka, float s): kd(_kd), ks(_ks), ka(_ka), shine(s) { }
 };
 
 class Light {
 public:
 	vec3 La, Le;
-	vec4 wLightPos;
-	vec4 wLightPosOfMyPair;
+	vec4 pos, posCurr;
+	vec4 posPair;
 
-	vec4 qmul(vec4 q1, vec4 q2) {
-		vec3 d1(q1.y, q1.z, q1.w), d2(q2.y, q2.z, q2.w);
-		vec3 res = q1.x * d2 + q2.x * d1 + cross(d1, d2);
-		return vec4(q1.x * q2.x - dot(d1, d2), res.x, res.y, res.z);
-	}
-
-	vec4 qinv(vec4 q) {
-		return vec4(q.x, -q.y, -q.z, -q.w) / dot(q, q);
-	}
-
-	vec4 quaternion(float t) {
-		return vec4(cosf(t / 4), sinf(t / 4) * cosf(t) / 2, sinf(t / 4) * sinf(t) / 2, sinf(t / 4) * sqrtf(3 / 4));
-	}
-
-	Light(vec3 _La, vec3 _Le, vec4 wlp, vec4 wlpomp): La(_La), Le(_Le), wLightPos(wlp), wLightPosOfMyPair(wlpomp) {}
+	Light(vec3 _La, vec3 _Le, vec4 pos1, vec4 pos2): La(_La), Le(_Le), pos(pos1), posPair(pos2) { }
 
 	void Animate(float tstart, float tend) {
-		// move origo to my pair
+		// move pair to origo
 		vec4 origo = vec4(0, 0, 0, 1);
-		vec4 shift = wLightPosOfMyPair - origo;
-		wLightPos = wLightPos + shift;
+		vec4 shift = origo - posPair;
+		vec4 posShifted = pos + shift;
 
 		// turn
-		vec4 q = quaternion(tend - tstart);
-		vec4 res = qmul(qmul(q, vec4(0, wLightPos.x, wLightPos.y, wLightPos.z)), qinv(q));
-		wLightPos = vec4(res.y, res.z, res.w, wLightPos.w);
+		vec4 q = quat(tend);
+		vec4 res = qmul(qmul(q, vec4(posShifted.x, posShifted.y, posShifted.z, 0)), qinv(q));
+		posShifted = vec4(res.x, res.y, res.z, pos.w);
 
 		// get back
-		wLightPos = wLightPos - shift;
+		posCurr = posShifted - shift;
 	}
 };
 
 struct RenderState {
 	mat4 MVP, M, Minv, V, P;
-	Material* material;
+	Material* material = nullptr;
 	std::vector<Light*> lights;
 	vec3 wEye;
 };
+
 class Shader : public GPUProgram {
 public:
 	virtual void Bind(RenderState state) = 0;
@@ -144,15 +172,16 @@ public:
 		setUniform(material.kd, name + ".kd");
 		setUniform(material.ks, name + ".ks");
 		setUniform(material.ka, name + ".ka");
-		setUniform(material.shininess, name + ".shininess");
+		setUniform(material.shine, name + ".shininess");
 	}
 
 	void setUniformLight(const Light& light, const std::string& name) {
 		setUniform(light.La, name + ".La");
 		setUniform(light.Le, name + ".Le");
-		setUniform(light.wLightPos, name + ".wLightPos");
+		setUniform(light.posCurr, name + ".wLightPos");
 	}
 };
+
 class PhongShader : public Shader {
 	const char* vertexSource = R"(
 		#version 330
@@ -218,7 +247,7 @@ class PhongShader : public Shader {
 			vec3 N = normalize(wNormal);
 			vec3 V = normalize(wView); 
 			if (dot(N, V) < 0) N = -N;	// prepare for one-sided surfaces like Mobius or Klein
-			float darken = (vtxPosi.z < 0) ? pow(0.5, floor(-vtxPosi.z / 1.5)) : 1;
+			float darken = (vtxPosi.z < 0) ? pow(0.5, floor(-vtxPosi.z)) : 1;
 			vec3 ka = material.ka * darken;
 			vec3 kd = material.kd * darken;
 
@@ -250,6 +279,7 @@ public:
 		}
 	}
 };
+
 class Geometry {
 protected:
 	unsigned int vao, vbo;
@@ -271,6 +301,7 @@ class ParamSurface : public Geometry {
 	struct VertexData {
 		vec3 position, normal;
 	};
+
 	unsigned int nVtxPerStrip, nStrips;
 public:
 	ParamSurface() { nVtxPerStrip = nStrips = 0; }
@@ -284,11 +315,11 @@ public:
 		eval(U, V, X, Y, Z);
 		vtxData.position = vec3(X.f, Y.f, Z.f);
 		vec3 drdU(X.d.x, Y.d.x, Z.d.x), drdV(X.d.y, Y.d.y, Z.d.y);
-		vtxData.normal = cross(drdU, drdV);
+		vtxData.normal = normalize(cross(drdU, drdV));
 		return vtxData;
 	}
 
-	void create(int N = tessellationLevel, int M = tessellationLevel) {
+	void create(int N = TESSELATION_LEVEL, int M = TESSELATION_LEVEL) {
 		nVtxPerStrip = (M + 1) * 2;
 		nStrips = N;
 		std::vector<VertexData> vtxData;
@@ -298,7 +329,7 @@ public:
 				vtxData.push_back(GenVertexData((float)j / M, (float)(i + 1) / N));
 			}
 		}
-		glBufferData(GL_ARRAY_BUFFER, nVtxPerStrip * nStrips * sizeof(VertexData), &vtxData[0], GL_STATIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, nVtxPerStrip * nStrips * sizeof(VertexData), &vtxData[0], GL_DYNAMIC_DRAW);
 		glEnableVertexAttribArray(0);
 		glEnableVertexAttribArray(1);
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(VertexData), (void*)offsetof(VertexData, position));
@@ -320,27 +351,9 @@ public:
 	}
 };
 
-struct Weight {
-	Dnum2 M, X, Y;
-
-	Weight(Dnum2& _M, Dnum2& _X, Dnum2& _Y): X(_X), Y(_Y), M(_M) { }
-};
-
-std::vector<Weight> weights;
-const Dnum2 r0 = Dnum2(0.0075f);
-
 class Sheet : public ParamSurface {
 public:
 	Sheet() { create(); }
-
-	Dnum2 h(Dnum2 X, Dnum2 Y) {
-		Dnum2 sum = Dnum2(0);
-		for (const auto& weight : weights) {
-			Dnum2 sqrtRes = Pow((X - weight.X) * (X - weight.X) + (Y - weight.Y) * (Y - weight.Y), 0.5f);
-			sum = sum + Dnum2(weight.M) / (sqrtRes + r0);
-		}
-		return sum * (-1);
-	}
 
 	void eval(Dnum2& U, Dnum2& V, Dnum2& X, Dnum2& Y, Dnum2& Z) {
 		X = U;
@@ -349,6 +362,8 @@ public:
 	}
 };
 
+Sphere* sphere;
+Sheet* sheet;
 PhongShader* shader;
 
 struct Object {
@@ -379,54 +394,104 @@ public:
 		geometry->Draw();
 	}
 
-	virtual void Animate(float tstart, float tend) { rotationAngle = 0.8f * tend; }
+	virtual void Animate(float tstart, float tend) { }
 };
 
-float rnd() { return (float)rand() / RAND_MAX; }
+struct SheetObject : Object {
+	SheetObject(Material* _material) : Object(_material, sheet) {
+		scale = { UPPER_BOUND * 2, UPPER_BOUND * 2, 1 };
+		translation = { -UPPER_BOUND, -UPPER_BOUND, 0 };
+	}
+};
 
-Sheet* sheet;
+struct SphereObject : Object {
+	vec3 v, normal;
+	float E;
+
+	SphereObject(Material* _material) : Object(_material, sphere), E(0), v({ 0, 0, 0 }), normal({ 0, 0, 1 }) { }
+
+	float CalcNormal() {
+		// get normal on sheet
+		vec3 p = (translation / UPPER_BOUND + vec3(1, 1, 1)) / 2.0f;
+		Dnum2 X = { p.x, vec2(1, 0) };
+		Dnum2 Y = { p.y, vec2(0, 1) };
+		Dnum2 Z = h(X, Y);
+		normal = normalize({ -Z.d.x, -Z.d.y, 1 });
+		return Z.f;
+	}
+
+	float CalcEnergy() {
+		return SPHERE_MASS * length(GRAVITY_VEC) * fabsf(MAX_DEPTH - translation.z) + 0.5f * SPHERE_MASS * dot(v, v);
+	}
+
+	void SetStartVec(vec3 _v) {
+		v = _v * AMPLIFIER;
+		E = CalcEnergy();
+	}
+
+	void Animate(float tstart, float tend) {
+		if (translation.z <= MAX_DEPTH)
+			return;
+
+		float dt = tend - tstart;
+
+		// Euler's
+		CalcNormal();
+		vec3 a = GRAVITY_VEC - dot(GRAVITY_VEC, normal) * normal;
+		v = v + a * dt;
+		translation = translation + v * dt;
+
+		// flat torus topology
+		if (translation.x >= UPPER_BOUND)
+			translation.x = translation.x - UPPER_BOUND * 2;
+
+		if (translation.y >= UPPER_BOUND)
+			translation.y = translation.y - UPPER_BOUND * 2;
+
+		// bring back to sheet	
+		float z = CalcNormal();
+		translation = vec3(translation.x, translation.y, z); //+ normal * SPHERE_MASS;
+
+		// energy correction
+		float expectedVelocity = sqrtf(2 * (E - length(GRAVITY_VEC) * fabsf(MAX_DEPTH - translation.z)));
+		float rate = expectedVelocity / length(v);
+		v = v * rate;
+		printf("E: %g\tv: %g (%g %g %g)\ta: %g (%g %g %g)\r", CalcEnergy(), length(v), v.x, v.y, v.z, length(a), a.x, a.y, a.z);
+	}
+};
 
 class Scene {	
-	std::vector<Object*> balls;
-	Object* sheetObject;
+	std::vector<SphereObject*> balls;
+	SheetObject* sheetObject = nullptr;
 	Camera camera;
 	std::vector<Light*> lights;
 public:
-	void BuildOneSphere() {
-		Material* material1 = new Material;
-		material1->kd = vec3(rnd(), rnd(), rnd());
-		material1->ks = vec3(1, 1, 1);
-		material1->ka = vec3(0.1f, 0.1f, 0.1f);
-		material1->shininess = 30;
-
-		Object* sphereObject = new Object(material1, new Sphere());
-		sphereObject->scale = vec3(0.5f, 0.5f, 0.5f);
-		sphereObject->translation = vec3(-9.5f, -9.5f, 0.25f);
+	SphereObject* BuildOneSphere() {
+		Material* mat = new Material({ rnd(), rnd(), rnd() }, { 1, 1, 1 }, { 0.1f, 0.1f, 0.1f }, 30);
+		SphereObject* sphereObject = new SphereObject(mat);
+		sphereObject->scale = { SPHERE_RADIUS, SPHERE_RADIUS, SPHERE_RADIUS };
+		sphereObject->translation = { -UPPER_BOUND + 1.5f * SPHERE_RADIUS, -UPPER_BOUND + 1.5f * SPHERE_RADIUS, SPHERE_RADIUS };
 		balls.push_back(sphereObject);
+		return sphereObject;
+	}
+
+	void StartSphere(SphereObject* sphereObject, vec3 startVec) {
+		sphereObject->SetStartVec(startVec);
 	}
 
 	void Build() {
-		Material* material2 = new Material;
-		material2->kd = vec3(0.2f, 0.5f, 0.8f);
-		material2->ks = vec3(0.6f, 0.6f, 0.6f);
-		material2->ka = vec3(0.1f, 0.1f, 0.1f);
-		material2->shininess = 30;
+		Material* mat = new Material({ 0.2f, 0.5f, 0.8f }, { 0.6f, 0.6f, 0.6f }, { 0.1f, 0.1f, 0.1f }, 30);
+		sheetObject = new SheetObject(mat);
+		
+		float z = UPPER_BOUND / tanf(M_PI * 22.5f / 180.0f);
+		camera.wEye = { 0, 0, z };
+		camera.wLookat = { 0, 0, 0 };
+		camera.wVup = { 0, 1, 0 };
 
-		BuildOneSphere();
-
-		sheet = new Sheet();
-		sheetObject = new Object(material2, sheet);
-		sheetObject->scale = vec3(30, 30, 1);
-		sheetObject->translation = vec3(-15, -15, 0);
-
-		camera.wEye = vec3(0, 0, 24.142f);
-		camera.wLookat = vec3(0, 0, 0);
-		camera.wVup = vec3(0, 1, 0);
-
-		vec4 pos1 = vec4(5, 5, 10, 1);
-		vec4 pos2 = vec4(-5, -5, 10, 1);
-		lights.push_back(new Light(vec3(0, 0, 0), vec3(1, 1, 1), pos1, pos2));
-		lights.push_back(new Light(vec3(0, 0, 0), vec3(1, 1, 1), pos2, pos1));
+		vec4 pos1 = { UPPER_BOUND * 1.5f, -UPPER_BOUND * 1.5f, z, 1 };
+		vec4 pos2 = { -UPPER_BOUND * 1.5f, UPPER_BOUND * 1.5f, z, 1 };
+		lights.push_back(new Light({ 0, 0, 0 }, { 1, 1, 1 }, pos1, pos2));
+		lights.push_back(new Light({ 0, 0, 0 }, { 1, 1, 1 }, pos2, pos1));
 	}
 
 	void Render() {
@@ -442,9 +507,33 @@ public:
 		sheetObject->Draw(state);
 	}
 
+	void Render(SphereObject* sphereObject) {
+		RenderState state;
+		Camera camera;
+		camera.wEye = sphereObject->translation + vec3(0, 0, SPHERE_RADIUS);
+		camera.wLookat = sphereObject->v == vec3(0, 0, 0)?
+			vec3(10, 10, SPHERE_RADIUS) : 
+			sphereObject->translation + normalize(sphereObject->v) + vec3(0, 0, SPHERE_RADIUS);
+		camera.wVup = sphereObject->normal;
+
+		// printf("wEye: (%g %g %g)\twLookat: (%g %g %g)\twVup: (%g %g %g)\r", camera.wEye.x, camera.wEye.y, camera.wEye.z, camera.wLookat.x, camera.wLookat.y, camera.wLookat.z, camera.wVup.x, camera.wVup.y, camera.wVup.z);
+
+		state.wEye = camera.wEye;
+		state.V = camera.V();
+		state.P = camera.P();
+		state.lights = lights;
+
+		for (auto& obj : balls) {
+			if (obj != sphereObject)
+				obj->Draw(state);
+		}
+		sheetObject->Draw(state);
+	}
+
 	void Animate(float tstart, float tend) {
 		for (auto& obj : balls) {
-			obj->Animate(tstart, tend);
+			if (!(obj->v == vec3(0, 0, 0)))
+				obj->Animate(tstart, tend);
 		}
 
 		for (auto& light : lights) {
@@ -455,45 +544,56 @@ public:
 
 bool isCameraAttached = false;
 Scene scene;
+SphereObject* nextSphere;
+SphereObject* renderedSphere;
 
 void onInitialization() {
 	glViewport(0, 0, windowWidth, windowHeight);
 	glEnable(GL_DEPTH_TEST);
 	glDisable(GL_CULL_FACE);
 	shader = new PhongShader();
+	sphere = new Sphere();
+	sheet = new Sheet();
 	scene.Build();
+	nextSphere = scene.BuildOneSphere();
 }
 
 void onDisplay() {
 	glClearColor(0, 0, 0, 1);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	scene.Render();
+	if (isCameraAttached)
+		scene.Render(renderedSphere);
+	else
+		scene.Render();
 	glutSwapBuffers();
 }
 
 void onKeyboard(unsigned char key, int pX, int pY) {
 	if (key == ' ') {
+		renderedSphere = nextSphere;
 		isCameraAttached = !isCameraAttached;
+		glutPostRedisplay();
 	}
 }
 
 void onKeyboardUp(unsigned char key, int pX, int pY) { }
 
-float startingMass = 0.05f;
+float mass = STARTING_MASS;
 
 void onMouse(int button, int state, int pX, int pY) {
 	float cX = 2.0f * pX / windowWidth - 1.0f;
 	float cY = 1.0f - 2.0f * pY / windowHeight;
+	float x = (cX + 1) / 2.0f;
+	float y = (cY + 1) / 2.0f;
 
 	if (state == GLUT_DOWN && button == GLUT_LEFT_BUTTON) {
-
+		scene.StartSphere(nextSphere, { x, y, 0 });
+		nextSphere = scene.BuildOneSphere();
 	}
 
 	if (state == GLUT_DOWN && button == GLUT_RIGHT_BUTTON) {
-		Dnum2 xd = Dnum2((cX + 1) / 2.0f);
-		Dnum2 yd = Dnum2((cY + 1) / 2.0f);
-		weights.push_back(Weight(Dnum2(startingMass), xd, yd));
-		startingMass *= 1.5f;
+		weights.push_back(Weight(mass, Dnum2(x), Dnum2(y)));
+		mass *= 1.25f;
 		sheet->create();
 		glutPostRedisplay();
 	}
@@ -508,7 +608,7 @@ void onIdle() {
 	tend = glutGet(GLUT_ELAPSED_TIME) / 1000.0f;
 
 	for (float t = tstart; t < tend; t += dt) {
-		float Dt = fmin(dt, tend - t);
+		float Dt = fminf(dt, tend - t);
 		scene.Animate(t, t + Dt);
 	}
 	glutPostRedisplay();
